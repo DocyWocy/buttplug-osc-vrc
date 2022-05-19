7f51c201fd7dc4f3150c5ce15d725c7b7273a022
DocyWocy <105699063+DocyWocy@users.noreply.github.com>
2022-05-18 23:21:17 -0400

    Update main.rs
    commands working
    testing out patterns

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ use buttplug::{
              device::VibrateCommand},
     connector::{ButtplugRemoteClientConnector, ButtplugWebsocketClientTransport},
     core::messages::serializer::ButtplugClientJSONSerializer,
+    //core::messages::SingleMotorVibrateCmd,
+    //core::messages::ButtplugDeviceCommandMessageUnion::SingleMotorVibrateCmd,
+    //core::messages::ButtplugDeviceMessageType::SingleMotorVibrateCmd,
 };
 use anyhow::{bail, Result, Error};
 use tracing::{debug, info, warn, error};
+use std::{thread, time};
+use std::collections::HashMap;
+
 
 const DEVICES_ALL: &str = "all";
 const DEVICES_LAST: &str = "last";
@@ -32,22 +38,24 @@ struct CliArgs {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+
     let args = CliArgs::from_args();
     tracing_subscriber::fmt()
         .with_ansi(false)
         .with_env_filter(tracing_subscriber::EnvFilter::new(args.rust_log))
         .with_thread_names(true)
         .init();
-
+    info!("main!");
     let osc_listen_host_port = validate_osc_listen_url(&args.osc_listen);
     let (devices_r, devices_w) = evmap::new();
     task::spawn_blocking(move || {
         info!("Starting OSC Server ({})", osc_listen_host_port);
         osc_listen(&osc_listen_host_port, devices_r);
     });
-
     let devices_m = Arc::new(Mutex::new(devices_w));
+    info!("main! devices_m");
     loop {
+        info!("main! loop!");
         let address = String::from(args.intiface_connect.as_str());
         let devices = devices_m.clone();
         let _ = task::spawn(intiface_connect(address, devices)).await;
@@ -130,6 +138,30 @@ fn osc_listen(host_port: &str, devices: evmap::ReadHandle<&'static str, Device>)
                                     )
                                 })
                             }
+                            Command::Test(index) => {
+                                task::spawn(async move {
+                                    debug!("[{}] testing", device_name);
+                                    println!("tested");
+                                    let mut feature :u32 = index as u32;
+                                    let millis = time::Duration::from_millis(33); //30.3hz update rate 30 times a second
+
+                                    thread::sleep(millis);
+
+                                    for x in 1..150 { // about 10 seconds? ramp up then stop
+                                        speed_cmd = 1.0/(x as f64); //SingleMotorVibrateCmd
+                                        //SingleMotorVibrateCmd::new(1,speed_cmd);
+                                        //SingleMotorVibrateCmd();DeviceIndex
+                                        //let s: HashMap<u32, f64> = {index, speed_cmd};
+
+                                        device.vibrate(VibrateCommand::SpeedMap(HashMap::from([(feature, speed_cmd)]))).await.map_err(|e| error!("{:?}", e)).expect("unable to perform VibrateCommand::SpeedMap");
+                                        //device.vibrate(VibrateCommand::Speed(speed_cmd)).await.map_err(|e| error!("{:?}", e));
+                                        thread::sleep(millis);
+                                    }
+
+                                    thread::sleep(millis);
+                                    device.vibrate(VibrateCommand::SpeedMap(HashMap::from([(feature, 0.0)]))).await.map_err(|e| error!("{:?}", e))
+                                })
+                            }
                         };
                     }
                 }
@@ -160,15 +192,56 @@ fn validate_osc_message(message: osc::Message) -> Option<CommandBroadcast> {
         warn!("[{}] {}", message.addr, error);
         None::<CommandBroadcast>
     };
+    let pass_error = |error: &str| {
+        //warn!("",);
+        None::<CommandBroadcast>
+    };
+    /*
+    string = message.addr.split('/');
+    for s in string {
+        //info!("{}", s);
+    }*/
+
     match path.get(3) { //device
         Some(&"devices") => {
             match path.get(5) { //skips name and gets <command>
                 Some(&"stop") => {
+
                     debug!("[{}]", message.addr);
                     Some(CommandBroadcast {
-                        devices_set: String::from(path[2]),
+                        devices_set: String::from(path[4]),
                         command: Command::Stop,
                     })
+                }
+                Some(&"test") => {
+                    match path.get(6) { //gets <argument>
+                        Some(&"index") => {
+                            match message.args {
+                                Some(ref message_args) => {
+                                    let index: i32 = match message_args.get(0) {
+                                        Some(OscType::Int(x)) => {
+                                            *x
+                                        }
+                                        Some(OscType::Int(x)) => {
+                                            (*x).into()
+                                        }
+                                        _ => {
+                                            return invalid(&format!("invalid argument value: {:?}", message_args[0]));
+                                        }
+                                    };
+                                    debug!("[{}] {}", message.addr, index);
+                                    debug!("[{}]", message.addr);
+                                    Some(CommandBroadcast {
+                                        devices_set: String::from(path[4]),
+                                        command: Command::Test(index),
+                                    })
+                                }
+                                None => invalid("invalid argument value: none")
+                            }
+                        }
+                        _ => invalid("invalid argument name")
+                    }
+
                 }
                 Some(&"vibrate") => {
                     match path.get(6) { //gets <argument>
@@ -188,7 +261,7 @@ fn validate_osc_message(message: osc::Message) -> Option<CommandBroadcast> {
                                     };
                                     debug!("[{}] {}", message.addr, speed);
                                     Some(CommandBroadcast {
-                                        devices_set: String::from(path[2]),
+                                        devices_set: String::from(path[4]),
                                         command: Command::Vibrate(speed),
                                     })
                                 }
@@ -201,7 +274,7 @@ fn validate_osc_message(message: osc::Message) -> Option<CommandBroadcast> {
                 _ => invalid("invalid command")
             }
         }
-        _ => invalid("invalid message")
+        _ => pass_error("invalid message")
     }
 }
 
@@ -218,10 +291,11 @@ fn validate_osc_listen_url(osc_listen_url: &Url) -> String {
 }
 
 type Speed = f64;
-
+type Index = i32;
 enum Command {
     Stop,
     Vibrate(Speed),
+    Test(Index),
 }
 
 struct CommandBroadcast {
